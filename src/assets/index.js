555607d995cc42396dd0b0a3cda966c9ebae6ee9
Luis carlos <luisxhl7@gmail.com>
2024-02-25 08:45:35 -0500

    adding styles of pagelayout and navBar

---
+++ src/assets/index.js
@@ -0,0 +1,14 @@
+import  incognitoPng from "./incognito.png";
+import  incognitoSvg from "./incognito.svg";
+import  pikaTriste from "./pika-triste.svg";
+import  pokeball from "./pokeball.png";
+
+
+const images = {
+    incognitoPng,
+    incognitoSvg,
+    pikaTriste,
+    pokeball,
+}
+
+export default images
---
+++ src/components/molecules/navBar/NavBar.jsx
@@ -0,0 +1,50 @@
+"use client"
+
+import React, { useEffect, useState } from 'react'
+import Link from 'next/link';
+import { Visibility, VisibilityOff } from '@mui/icons-material';
+import images from '@/assets';
+import './NavBar.scss'
+import Image from 'next/image';
+
+
+const handleViewPokemons = () => {
+  const newValue = !JSON.parse(localStorage.getItem('viewPokemons'));
+  localStorage.setItem('viewPokemons', JSON.stringify(newValue));
+  window.location.reload();
+};
+
+export const NavBar = () => {
+  const [viewPokemons, setViewPokemons] = useState(false);
+
+  useEffect(() => {
+    const storedViewPokemons = localStorage.getItem('viewPokemons');
+    
+    if (storedViewPokemons !== null) {
+      setViewPokemons(JSON.parse(storedViewPokemons));
+    }
+
+  }, []);
+  
+  return (
+    <nav className='navbar'>
+
+      <Link href='/'>
+        <Image src={images.pokeball} alt="Pokeball" width={40} height={40} />
+        Pokedex
+      </Link>
+      
+      <button type='button'
+        onClick={handleViewPokemons} 
+        className='navbar__button-view-pokemons'
+        title={viewPokemons  ? 'Ocultar Pokemons' : 'Mostrar Pokemons'}
+      >
+        {viewPokemons ?
+          <VisibilityOff/>
+          :
+          <Visibility/>
+        }
+      </button>
+    </nav>
+  )
+}
