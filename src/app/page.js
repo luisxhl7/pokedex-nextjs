4c3f419cdc257f3c00a0c468216e1b5cddc24020
Luis carlos <luisxhl7@gmail.com>
2024-02-25 09:05:28 -0500

    adding componente of search

--- src/app/page.js
+++ src/app/page.js
@@ -1,8 +1,36 @@
+'use client'
+
+import { Search } from "@mui/icons-material";
+import { useForm } from "@/hooks/useForm";
+import './PokemonsPage.scss'
+
+const formData = {
+  pokemon: '',
+}
 
 
-export default function Home() {
+const PokemonsPage = () => {
+  const { pokemon, onInputChange } = useForm(formData)
+
   return (
-    <>
-    </>
+    <section className='PokemonsPage'>
+      
+      <form autoComplete='off'>
+        <div className='PokemonsPage__content-search'>
+          <Search className='PokemonsPage__content-search__icon-search'/>
+          <input
+            type="text"
+            name="pokemon"
+            className='PokemonsPage__input'
+            onChange={onInputChange}
+            value={pokemon}
+            placeholder='Busca por número o nombre'
+          />      
+        </div>
+      </form>
+    
+    </section>
   );
 }
+
+export default PokemonsPage
---
+++ src/hooks/useForm.jsx
@@ -0,0 +1,59 @@
+import { useEffect, useMemo, useState } from 'react';
+
+export const useForm = ( initialForm = {}, formValidations = {} ) => {
+  
+    const [ formState, setFormState ] = useState( initialForm );
+    const [ formValidation, setFormValidation ] = useState({});
+
+    useEffect(() => {
+        createValidators()
+        // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, [formState])
+
+    useEffect(() => {
+        setFormState(initialForm)
+    }, [initialForm])
+    
+    const onInputChange = ({ target }) => {
+        const { name, value } = target;
+        setFormState({
+            ...formState,
+            [ name ]: value
+        });
+    }
+
+    const onResetForm = () => {
+        setFormState( initialForm );
+    }
+
+    const createValidators = () => {
+        const formCheckedValues = {}
+
+        for (const formField of Object.keys(formValidations)) {
+            const [fn, errorMessage] = formValidations[formField]
+
+            formCheckedValues[`${ formField }Valid`] = fn( formState[formField]) ? null : errorMessage
+        }
+        setFormValidation(formCheckedValues)
+    }
+
+    const isFormValid = useMemo( () => {
+        formValidation
+        for (const formValue of Object.keys( formValidation )) {
+            if (formValidation[formValue] !== null) {
+                return false
+            }
+        }
+        
+        return true
+    },[formValidation])
+
+    return {
+        ...formState,
+        formState,
+        onInputChange,
+        onResetForm,
+        isFormValid,
+        ...formValidation
+    }
+}
